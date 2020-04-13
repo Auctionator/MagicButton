bef47fe3777c3fbe0439962d2b562c066a09f9fc
plusmouse <plusmouse@protonmail.com>
2020-04-13 21:32:17 +0100

    Autocancel variant

--- MagicButton.lua
+++ MagicButton.lua
@@ -17,9 +17,58 @@ function MagicButton_Print(message)
 end
 
 local auctionToCancel = nil
-local function CancelAuctionMagicEvent(frame, event)
+local cancel = false
+local auctionCancelIndex = 1
+local MAGIC_BUTTON_CANCEL_EVENTS = {
+  "AUCTION_HOUSE_SHOW",
+  "OWNED_AUCTIONS_UPDATED",
+  "COMMODITY_SEARCH_RESULTS_UPDATED",
+  "ITEM_SEARCH_RESULTS_UPDATED",
+}
+local function setCancelAuction()
+  local wasAuction = auctionToCancel
+  auctionToCancel = C_AuctionHouse.GetOwnedAuctionInfo(auctionCancelIndex)
+  if not auctionToCancel and wasAuction then
+    MagicButton_Print("No more to cancel")
+    auctionCancelIndex = 1
+  end
+  cancel = false
+end
+local function CancelAuctionMagicEvent(frame, event, itemKey)
+  if event == "AUCTION_HOUSE_SHOW" then
+    cancel = false
+    auctionCancelIndex = 1
+    return
+  end
   if event == "OWNED_AUCTIONS_UPDATED" then
-    auctionToCancel = C_AuctionHouse.GetOwnedAuctionInfo(1)
+    setCancelAuction()
+    return
+  end
+
+  if not auctionToCancel or cancel then
+    return
+  end
+
+  local resultInfo
+  local itemKeyInfo = C_AuctionHouse.GetItemKeyInfo(auctionToCancel.itemKey)
+  if itemKeyInfo.isCommodity then
+    resultInfo = C_AuctionHouse.GetCommoditySearchResultInfo(auctionToCancel.itemKey.itemID, 1)
+  else
+    resultInfo = C_AuctionHouse.GetItemSearchResultInfo(auctionToCancel.itemKey, 1)
+  end
+
+  if not resultInfo then
+    return
+  end
+
+  if resultInfo.owners[1] ~= "player" then
+    cancel = true
+    MagicButton_Print("Click again to cancel ".. itemKeyInfo.itemName)
+  else
+    cancel = false
+    MagicButton_Print("You own the top auction for ".. itemKeyInfo.itemName ..". Looking at next one")
+    auctionCancelIndex = auctionCancelIndex + 1
+    setCancelAuction()
   end
 end
 
@@ -27,14 +76,17 @@ function MagicButton_CancelAuctionMagic()
   if not MagicFrame then
     MagicFrame = CreateFrame("Frame",nil,UIParent)
     MagicFrame:SetScript("OnEvent", CancelAuctionMagicEvent)
-    MagicFrame:RegisterEvent("OWNED_AUCTIONS_UPDATED")
+    FrameUtil.RegisterFrameForEvents(MagicFrame, MAGIC_BUTTON_CANCEL_EVENTS)
     C_AuctionHouse.QueryOwnedAuctions({})
     MagicButton_Print("Auction cancelling loaded. Click once more please :)")
   end
-  if auctionToCancel then
-    AuctionHouseFrame.AuctionsFrame:SelectAuction(auctionToCancel)
-    AuctionHouseFrame.AuctionsFrame:CancelSelectedAuction()
-    StaticPopup1Button1:Click()
-    auctionToCancel = nil
+  if cancel then
+    MagicButton_Print("Cancelling ID " .. auctionToCancel.auctionID)
+    C_AuctionHouse.CancelAuction(auctionToCancel.auctionID)
+    cancel = false
+  elseif auctionToCancel then
+    C_AuctionHouse.SendSearchQuery(auctionToCancel.itemKey, {}, true)
+  else
+    setCancelAuction()
   end
 end
