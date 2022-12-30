3183d3866ff529efef41e2b0222a4d3426180f48
plusmouse <plusmouse@protonmail.com>
2022-12-30 17:24:01 +0000

    Update commodity buy code to be as fast as possible

--- Source/Mixins/CommodityBuy.lua
+++ Source/Mixins/CommodityBuy.lua
@@ -1,12 +1,12 @@
 MagicButtonCommodityBuyFrameMixin = {}
 
-local THROTTLE_EVENTS = {
-  "AUCTION_HOUSE_THROTTLED_SYSTEM_READY",
+local BUY_EVENTS = {
+  "COMMODITY_PRICE_UPDATED",
 }
 
 function MagicButtonCommodityBuyFrameMixin:OnEvent(event, ...)
-  if event == "AUCTION_HOUSE_THROTTLED_SYSTEM_READY" then 
-    local newAmount = AuctionHouseFrame.BuyDialog.PriceFrame:GetAmount()
+  if event == "COMMODITY_PRICE_UPDATED" then
+    local _, newAmount = ...
     local oldAmount = AuctionHouseFrame.CommoditiesBuyFrame.BuyDisplay.TotalPrice:GetAmount()
     if newAmount <= oldAmount then
       AuctionHouseFrame.BuyDialog.BuyNowButton:Click()
@@ -14,12 +14,12 @@ function MagicButtonCommodityBuyFrameMixin:OnEvent(event, ...)
       MagicButton_Print(MAGIC_BUTTON_L_COMMODITY_PRICE_INCREASED)
       AuctionHouseFrame.BuyDialog.CancelButton:Click()
     end
-    FrameUtil.UnregisterFrameForEvents(self, THROTTLE_EVENTS)
+    FrameUtil.UnregisterFrameForEvents(self, BUY_EVENTS)
   end
 end
 
 function MagicButtonCommodityBuyFrameMixin:ButtonPress()
   AuctionHouseFrame.CommoditiesBuyFrame.BuyDisplay.BuyButton:Click()
 
-  FrameUtil.RegisterFrameForEvents(self, THROTTLE_EVENTS)
+  FrameUtil.RegisterFrameForEvents(self, BUY_EVENTS)
 end
